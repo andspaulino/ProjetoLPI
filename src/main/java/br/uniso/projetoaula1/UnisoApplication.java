f3e8c5bd29230ea3d1f56de08804f98a1b4fcc0b
Anderson Paulino <andspaulino@gmail.com>
2020-09-05 00:01:13 -0300

    .gitignore fixing

---
+++ src/main/java/br/uniso/projetoaula1/UnisoApplication.java
@@ -0,0 +1,14 @@
+package br.uniso.projetoaula1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnisoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(UnisoApplication.class, args);
+    }
+
+}
